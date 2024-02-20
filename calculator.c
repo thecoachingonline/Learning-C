41202f8e7aa43018d0b36cdc2b2678dab34224b6
Y.Nanthachai <thecoachingonline@gmail.com>
2024-02-20 00:07:12 +0000

    Scope

--- calculator.c
+++ calculator.c
@@ -1,10 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int add(int a, int b);
+
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", x + y);
+    int z = add(x, y);
+    printf("%i\n", z);
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
