a1fed3d676768df2cc2e0cae1612f6e5f3c52908
Y.Nanthachai <thecoachingonline@gmail.com>
2024-03-05 05:59:10 +0000

    Artay of Structures

---
+++ Advanced/Structarray๒.c
@@ -0,0 +1,14 @@
+
+	struct weather {
+		char day[10];
+		float temp;
+	};
+	struct weather today = { "Sunday", 72.5 };
+
+	puts("Today's forecast:");
+	printf("%10s %.1f degrees\n",
+			today.day,
+			today.temp);
+
+	return(0);
+}
