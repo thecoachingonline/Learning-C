9eaf0adc71d842753c739979712eb71d8518463b
Y.Nanthachai <thecoachingonline@gmail.com>
2024-07-31 11:01:33 +0000

    ตัวแปร

--- SiPA/main.c
+++ SiPA/main.c
@@ -4,4 +4,14 @@
 int main() {
     printf("%d", plus(1,2));
     return 0;
+
+    /*
+    char c;
+    char c1 = 'a';
+    char c2 = '%';
+    char c3 = ' ';
+    char c4 = '7';
+    int circleArea1;
+    float _pi;
+    */  
 }
